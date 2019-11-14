d8408cd6b65e23e13b5234b8e9f976a3cd2ec20f
Duc Tan Lam <lamductan@gmail.com>
2019-11-14 19:26:02 +0700

    Update 4_install_packages.sh

--- 4_install_packages.sh
+++ 4_install_packages.sh
@@ -251,8 +251,8 @@ make -j
 make install
 cd $HOME
 curl http://archive.ubuntu.com/ubuntu/pool/main/b/bzip2/bzip2_1.0.6.orig.tar.bz2 -o $SOURCE_DIR/bzip2.tar.bz2
-## override current Makefile with Makefile added -fPIC
 tar xvjf $SOURCE_DIR/bzip2* -C $PACKAGE_DIR/
+## override current Makefile with Makefile added -fPIC
 cp $DIRECTORY/utils/bz2_Makefile $PACKAGE_DIR/bzip2*/Makefile
 cd $PACKAGE_DIR/bzip2*
 make -j
