70e288954129294f9577672a49cd0b7c9bc76957
Duc Tan Lam <lamductan@gmail.com>
2019-11-14 04:47:06 -0600

    Update 2_init.sh

--- 2_init.sh
+++ 2_init.sh
@@ -3,4 +3,5 @@ mkdir $HOME/.tmp
 wget "https://www.dropbox.com/sh/1bowihot6muvopb/AACyEAJceFCUAdSFv7RPO48ma?dl=0" -O utils/archives.zip
 unzip utils/archives.zip -d utils/archives
 cp utils/dotfiles/bashrc $HOME/.bashrc
+bash utils/gen_gen_pugixml_pkgconfig.sh
 bash utils/config_path.sh
