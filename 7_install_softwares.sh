e40471e2b1e5f28675fb80370cb37a07781f52b6
Duc Tan Lam <lamductan@gmail.com>
2019-11-16 09:53:50 -0600

    Add files via upload

--- 7_install_softwares.sh
+++ 7_install_softwares.sh
@@ -6,16 +6,18 @@ PROGRAM_DIR=$HOME/Downloads/programs
 PACKAGE_DIR=$HOME/packages
 OPT_DIR=$LOCAL_DIR/opt
 APP_DIR=$LOCAL_DIR/share/applications
+APP_ICON_DIR=$LOCAL_DIR/share/icons
 DIRECTORY=$(cd `dirname $0` && pwd)
 
 mkdir $PROGRAM_DIR
+mkdir $APP_ICON_DIR
 
 ### Google chrome
 wget http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_78.0.3904.87-1_amd64.deb --no-check-certificate -O $PROGRAM_DIR/google-chrome-stable_78.0.3904.87-1_amd64.deb
 dpkg -x $PROGRAM_DIR/google-chrome-stable_78.0.3904.87-1_amd64.deb $LOCAL_DIR
 ln -s $LOCAL_DIR/opt/google/chrome/chrome $LOCAL_DIR/bin/chrome
 chmod +x $LOCAL_DIR/bin/chrome
-cp $LOCAL_DIR/opt/google/chrome/product_logo_256.png $HOME/.local/share/icons/chrome.png
+cp $LOCAL_DIR/opt/google/chrome/product_logo_256.png $APP_ICON_DIR/chrome.png
 cp $DIRECTORY/utils/desktop/chrome.desktop $APP_DIR
 chmod a+x $APP_DIR/chrome.desktop
 cd $HOME
@@ -26,7 +28,7 @@ curl https://az764295.vo.msecnd.net/stable/86405ea23e3937316009fc27c9361deee66ff
 dpkg -x $PROGRAM_DIR/code_1.40.0-1573064499_amd64.deb $LOCAL_DIR
 ln -s $LOCAL_DIR/usr/share/code/code $LOCAL_DIR/bin/code
 cp $DIRECTORY/utils/desktop/code.desktop $APP_DIR
-cp $LOCAL_DIR/usr/share/pixmaps/com.visualstudio.code.png $LOCAL_DIR/share/icons/com.visualstudio.code.png
+cp $LOCAL_DIR/usr/share/pixmaps/com.visualstudio.code.png $APP_ICON_DIR/com.visualstudio.code.png
 cd $HOME
 
 ### skype
@@ -36,7 +38,7 @@ cp $DIRECTORY/utils/desktop/code.desktop $APP_DIR
 cp $LOCAL_DIR/usr/share/pixmaps/com.visualstudio.code.png $LOCAL_DIR/share/icons/com.visualstudio.code.png
 ln -s $LOCAL_DIR/usr/share/skypeforlinux/skypeforlinux $LOCAL_DIR/bin/skypeforlinux
 cp $DIRECTORY/utils/desktop/skypeforlinux.desktop $APP_DIR
-cp $LOCAL_DIR/usr/share/pixmaps/skypeforlinux.png $LOCAL_DIR/share/icons/skypeforlinux.png
+cp $LOCAL_DIR/usr/share/pixmaps/skypeforlinux.png $APP_ICON_DIR/skypeforlinux.png
 cd $HOME
 
 ### sublime-text
@@ -44,7 +46,7 @@ curl https://download.sublimetext.com/sublime_text_3_build_3211_x64.tar.bz2 -o $
 tar xvjf $PROGRAM_DIR/sublime_text_3_build_3211_x64.tar.bz2 -C $OPT_DIR
 ln -s $OPT_DIR/sublime_text_3/sublime_text $LOCAL_DIR/bin/sublime_text
 cp $DIRECTORY/utils/desktop/sublime_text.desktop $APP_DIR
-cp $OPT_DIR/sublime_text_3/Icon/256x256/sublime-text.png $LOCAL_DIR/share/icons/sublime-text.png
+cp $OPT_DIR/sublime_text_3/Icon/256x256/sublime-text.png $APP_ICON_DIR/sublime-text.png
 cd $HOME
 
 ### telegram
@@ -57,7 +59,7 @@ curl https://downloads.slack-edge.com/linux_releases/slack-desktop-4.1.2-amd64.d
 dpkg -x $PROGRAM_DIR/slack-desktop-4.1.2-amd64.deb $LOCAL_DIR
 ln -s $LOCAL_DIR/usr/bin/slack $LOCAL_DIR/bin/slack
 cp $DIRECTORY/utils/desktop/slack.desktop $APP_DIR
-cp $LOCAL_DIR/usr/share/pixmaps/slack.png $LOCAL_DIR/share/icons/slack.png
+cp $LOCAL_DIR/usr/share/pixmaps/slack.png $APP_ICON_DIR/slack.png
 cd $HOME
 
 ### Intellij
